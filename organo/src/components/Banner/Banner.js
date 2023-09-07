a3a48cf5720e0297f3e877231cc3dfc493934d96
mizael <mizael.pardal@smartnx.com>
2023-09-06 21:54:13 -0300

    feat: criando componente Banner

---
+++ organo/src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+    return (
+        <header className="banner">
+            <img src="/images/banner.png" width='100%' ></img>
+        </header>
+    
+    )
+}
+
+export default Banner;
