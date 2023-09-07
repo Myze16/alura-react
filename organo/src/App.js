473a53439241040ed0017fe2c4019fc6eae904c1
mizael <mizael.pardal@smartnx.com>
2023-09-07 10:53:57 -0300

    feat: criando componente input de texto

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,27 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// Tip: para importar um componente precisamos acessar o seguinte caminho: 
+// ./components/{folderComponentName}/{fileComponentName}
+// Mas o react possui uma forma mais interessante de tratar isso, sem precisar
+// repetir o nome(pasta e arquivo) como normalmente ocorre, e podemos fazer 
+// isso da seguinte forma:
+// Basta colocar o nome do arquivo como index.js na pasta do componente e o
+// react vai buscar diretamente esse arquivo dentro da pasta que estamos importando.
+// Agora nossa importação fica da seguinte forma: ./components/{folderComponentName}
 
 import Banner from './components/Banner/Banner';
+import TextInput from './components/TextInput/TextInput';
 
 function App() {
   return (
-    <div className="App">
-      <Banner />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <Banner />
+          <TextInput label="Nome" placeholder="Digite seu nome" />
+          <TextInput label="Cargo" placeholder="Digite seu cargo" />
+          <TextInput label="Imagem" placeholder="Digite o endereço da imagem" />
+      </div>
   );
 }
 
